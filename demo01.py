31533114f243dc995634c6214572daaee2a44b73
ran <fenyunkuang@gmail.com>
2021-07-26 19:42:37 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,61 @@
+""""
+print("hello world!")   #字符串 
+print(233)    #整数
+print(2.333) #小数
+print(True)   #布尔值 True False
+print(())   #元祖
+print([])    #数组
+print({})  #字典
+
+锄禾日当午
+汗滴禾下土
+
+print("哈哈",22333)
+print("哈哈"+"嘻嘻") #字符串的拼接
+print("哈哈"*100)
+print(((1+2)*100)/2)
+print(2>3)
+a = "张三" #把张三的这个值赋值给了名字叫a的变量
+print(a)
+
+"""
+# a = input("请输入：")
+# b = input("请输入：")
+# print("input获取的内容",a+b)
+
+# # 数据格式的转换
+# c = type(233)
+# print(c)
+# print(type("哈哈"))
+# print(type(233))
+# print(type(2.33))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+# print(type(True))
+
+
+# a = "2333"
+# print(type(a))
+
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容",a+b)
+
+# a = "哈哈哈"
+# print(len(a))
+
+"""
+练习：通过代码获取两段内容，并且计算它们的长度和。
+"""
+
+
+# a = input("请输入第一次你想说的话:")
+# b = input("请输入第二次你想说的话:")
+# print(len(a)+len(b))
+# # print(len(a)+len(b))
+
+a = input("请输入：")
+b = input("请输入：")
+print("两段字符串的长度是：",len(a)+len(b))
